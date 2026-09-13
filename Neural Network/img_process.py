7843d765292c8d9f02595ac6dbdc3c9279fa259c
Thomas <thomas12flower@gmail.com>
2026-09-13 20:52:57 +0100

    feat: Preprocessing imgages to csv file

--- Neural Network/img_process.py
+++ Neural Network/img_process.py
@@ -4,75 +4,71 @@ This vector is what is used to train the neural network.
 Creates a new class that containts the data and the correct labbel for training
 '''
 
-from enum import Enum
-
 import numpy as np
 from PIL import Image
 
+WIDTH = 64
+HEIGHT = 64
 
-class Classification(Enum):
-    DOG = 0
-    CAT = 1
-
-
-class Data:
-    def __init__(self, data: np.array, classification: Classification):
-        self._data: np.array = data
-        self._classification: Classification = classification
-
-    def get_data(self) -> np.array:
-        return self._data
-
-    def set_data(self, vector: np.array) -> None:
-        # TODO need to make sure that the shape is correct
-        self._data = vector
-
-
-    def get_classification(self) -> Classification:
-        return self._classification
-
-    def set_classification(self, cls: Classification):
-        self._classification = cls
-
-
-def get_img_vector(rel_path: str, cls: Classification) -> Data:
-    '''
-    Takes in the relative path and returns the grayscale vector of the image 
-    This provides a single input to the nn
-    '''
-
+def validate(rel_path: str):
     img = Image.open(rel_path).convert("L") # convert the image to grayscale   
     data = np.array(img.getdata())
 
-    return Data(data, cls)
+   
+    width, height = img.size
+
+    matrix = []
+
+    index = 0
+    for _ in range(height):
+        row = []
+        for _ in range(width):
+            row.append(data[index])
+            index += 1
+        matrix.append(row)
+
+    for row in matrix:
+        for col in row:
+            if col > 128:
+                print(' ', end='')
+            else:
+                print('1', end='')
+
+        print('\n')
+
+def process(filename: str) -> None:
+    '''
+    Given a filenameto write to, this function will:
+    - Crop the image
+    - Turn the image grayscale
+    - Convert this data to a (1,) arr
+    - write this to a file
+
+    Note: This was written to process the images formatted "cat.n.jpg" etc
+    Note2: Writes to a csv file. The data can be read using the complimentary "load" function
+    '''
+    START_INDEX = 4001
+    END_INDEX = 4500
+
+    with open(filename, "w") as wf: # Outside of the loop so we don't constantly open the file
+        for i in range(START_INDEX, END_INDEX):
+            # fn = f"data_set/cats_set/cat.{i}.jpg"
+            fn = f"data_set/dogs_set/dog.{i}.jpg"
+            img = Image.open(fn).convert("L")
+
+            # Resizing the image 
+            resize = img.resize((WIDTH, HEIGHT))
+
+            data = list(resize.getdata()) # This is the list of grayscale values
+            wf.write(','.join(str(num) for num in data))
+            wf.write("\n")
+            
+
+### WRITING THE DATA
+# process("dog.csv")
 
 
-    ### VALIDATION ###
-    # WIDTH, HEIGHT = img.size
-
-    # matrix = []
-
-    # index = 0
-    # for _ in range(HEIGHT):
-    #     row = []
-    #     for _ in range(WIDTH):
-    #         row.append(data[index])
-    #         index += 1
-    #     matrix.append(row)
-
-    # for row in matrix:
-    #     for col in row:
-    #         if col > 128:
-    #             print(' ', end='')
-    #         else:
-    #             print('1', end='')
-
-    #     print('\n')
 
 
-### TODO: Need to do some iamge cleanup ###
-### TODO: Need to move the image to a standalone function ###
 
-data = get_img_vector("eggs.png", Classification.DOG)
-print(type(data.get_classification()))
 
