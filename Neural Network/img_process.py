5beff04f8db09ead3c55f5066adf49107ff9b808
Thomas <thomas12flower@gmail.com>
2026-09-20 20:10:55 +0100

    patch: Data class was using List not np.ndarray

--- Neural Network/img_process.py
+++ Neural Network/img_process.py
@@ -4,11 +4,11 @@ This vector is what is used to train the neural network.
 Creates a new class that containts the data and the correct labbel for training
 '''
 
-import numpy as np
 from PIL import Image
+import numpy as np
 
-WIDTH = 128
-HEIGHT = 128
+WIDTH = 256
+HEIGHT = 256
 
 def process(filename: str) -> None:
     '''
@@ -25,24 +25,28 @@ def process(filename: str) -> None:
     END_INDEX = 4500
 
     with open(filename, "w") as wf: # Outside of the loop so we don't constantly open the file
-        for i in range(START_INDEX, END_INDEX):
+        for i in range(START_INDEX, END_INDEX + 1):
             fn = f"data_set/cats_set/cat.{i}.jpg"
             # fn = f"data_set/dogs_set/dog.{i}.jpg"
             img = Image.open(fn).convert("L")
 
             # Resizing the image 
             resize = img.resize((WIDTH, HEIGHT))
-
             data = list(resize.getdata()) # This is the list of grayscale values
+            if len(data) != WIDTH * HEIGHT:
+                raise Exception("READ ERROR")
+
+
             wf.write(','.join(str(num) for num in data))
-            wf.write("\n")
+            # DONT PRINT A NEWLINE AT THE END OF THE FILE - CAUSED ISSUES FOR TRAINING
+            if i != END_INDEX:
+                wf.write("\n")
             
 
 class Data:
     def __init__(self, data, target):
-        self._data: list = data
-        self._target: list = target
-
+        self._data: np.ndarray = data
+        self._target: np.ndarray = target
 
     def get_data(self):
         return self._data
@@ -52,28 +56,41 @@ class Data:
 
 
 def read(filename: str, hot_encoded: list) -> list:
-    '''Takes in the csv filename and returns a list of Data objects'''
+    '''
+    Takes in the csv filename and returns a list of Data objects
+
+    # NOTE SHOULD NOT HAVE USED LISTS IN THE ORIGINAL IMPLEMENTATION
+    '''
 
     ### hot encoded is either [0, 1] or [1, 0 ] for our uses case
     sol = []
     with open(filename, "r") as rf:
         for line in rf:
-            arr =  arr = [int(x) / 255 for x in line.rstrip().split(",")]
+            arr = np.ndarray([int(x) / 255 for x in line.rstrip().split(",")])
             data = Data(arr, hot_encoded)
             sol.append(data)
 
     return sol
 
 
-
-### TESTING
-# data = read("cat_dog_training_data/cat.csv", [0, 1])
-
-
-
 ### WRITING THE DATA
-# process("dog.csv")
-process("cat.csv")
+if __name__ == "__main__":
+    pass
+    # process("dog.csv")
+    # process("cat.csv")
+
+    ### TESTING WHAT WOULD A RESONABLE IMAGE SIZE CONSIST OF ###
+    # lengths = []
+    # for i in range(4001, 4500, 1):
+    #     sample = f"data_set/cats_set/cat.{i}.jpg"
+    #     img = Image.open(sample).convert("L")
+    #     lengths.append(len(img.getdata()))
+
+    # avg = round(sum(lengths) / len(lengths))
+    # print(f"The average number of pixels is: {avg}")
+    # print(f"We are resizing to {WIDTH} X {HEIGHT} which is {WIDTH * HEIGHT} pixels")
+    # print(f"This is {round(WIDTH * HEIGHT / avg * 100)}% of the pixels of the original image")
+
 
 
 
